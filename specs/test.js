87db229b46d653befd42133977117d434eaee4dd
rkurtianyk <ruslan.kurtjanyk@gmail.com>
2018-12-11 01:43:31 +0200

    Add test for eleks main test

--- specs/test.js
+++ specs/test.js
@@ -1,8 +1,15 @@
-describe('angularjs homepage', function() {
-    it('should have a title', function() {
+require("../matchers");
 
-      browser.get('https://www.protractortest.org/');
+describe('Test examples', function() {
+    it('should open Eleks main page', async () => {
+      browser.waitForAngularEnabled(false);
+      
+      await browser.get('https://eleks.com/');
 
-      expect(browser.getTitle()).toContain('Protractor');
+      const languageSwitch = element(by.css('.head__lang'));
+      console.log(languageSwitch);
+      languageSwitch.click();
+
+      expect(languageSwitch).toHaveClass('active');
     });
-  });
+});
